e0c9a81471892c3437299f0d06fb08ff609b0846
Hectorv72 <hectorvaldezfsa13@gmail.com>
2023-02-24 10:31:26 -0300

    inicializa el proyecto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>My app</div>
+  )
+}
+
+export default App
