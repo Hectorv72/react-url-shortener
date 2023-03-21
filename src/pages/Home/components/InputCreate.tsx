a924dc9be1c79379f5011c98bf107ca7571aef57
Hectorv72 <hectorvaldezfsa13@gmail.com>
2023-03-21 12:45:25 -0300

    agrega funcionalidad de crear shortener

--- src/pages/Home/components/InputCreate.tsx
+++ src/pages/Home/components/InputCreate.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent, useRef, useState } from 'react'
+import { createShortener } from '@services/ShortenerServices';
+import { isUri } from 'valid-url'
 
-const InputCreate = () => {
+interface propsType {
+  onCreate: Function;
+}
+
+const InputCreate = ({ onCreate }: propsType) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [link, setLink] = useState<string | undefined>(undefined)
+  const [loading, setLoading] = useState(false)
+
+  const handleVerifyUrl = async (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    event.target.className = `form-control ${isUri(value) ? 'text-success' : 'text-danger'}`
+    isUri(value) ? setLink(value) : setLink(undefined)
+  }
+
+  const handleCreateShortener = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (link) {
+      setLoading(true)
+      const shortener = await createShortener(link)
+      if (shortener) {
+        if (inputRef.current?.value) { inputRef.current.value = '' }
+        onCreate && onCreate(shortener)
+      }
+      setLoading(false)
+    }
+  }
+
   return (
-    <div className='d-flex flex-row gap-2'>
-      <input type='url' className='form-control' />
-      <button type='button' className='btn btn-primary'>Convertir</button>
+    <div>
+      <form className='d-flex flex-row gap-2' onSubmit={handleCreateShortener}>
+        <input type='url' ref={inputRef} className='form-control' onChange={handleVerifyUrl} />
+        <button type='submit' disabled={loading} className='btn btn-primary'>Convertir</button>
+      </form>
     </div>
   )
 }
--- src/services/ShortenerServices.tsx
+++ src/services/ShortenerServices.tsx
@@ -1,8 +1,13 @@
 import ShortenerModel from "@models/ShortenerModel";
+import axios from "axios";
 
-export const createShortener = async (): Promise<any> => {
+const url = import.meta.env.VITE_APP_API_URL
+
+export const createShortener = async (link: string): Promise<ShortenerModel | null> => {
   try {
+    const response = await axios.post(`${url}/shortener`, { url: link })
+    return response.data
   } catch (error) {
-
+    return null
   }
 }
